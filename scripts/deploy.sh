4626924841cd41cae5045764cbfd070b87232990
n.ryzhkov <nikita.tester@bk.ru>
2025-08-31 19:27:53 +0300

    вывод в /dev/null

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-helm install my-redpanda redpanda-data/redpanda --version 25.1.1 -f helm/redpanda/values.yaml
-helm install pguser oci://registry-1.docker.io/bitnamicharts/postgresql -f helm/pguser/values.yaml
+helm install my-redpanda redpanda-data/redpanda --version 25.1.1 -f helm/redpanda/values.yaml > /dev/null
+helm install pguser oci://registry-1.docker.io/bitnamicharts/postgresql -f helm/pguser/values.yaml > /dev/null
 kubectl wait --for=condition=Ready pod/pguser-postgresql-primary-0 --timeout=2m
 
 kubectl exec my-redpanda-0 -it -- rpk topic create -p 1 new_users
